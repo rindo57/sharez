dec57a69824e3b5aca53fad5aba88cd302b5f9c6
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-10-15 19:17:29 +0530

    Update fileClickHandler.js

--- website/static/js/fileClickHandler.js
+++ website/static/js/fileClickHandler.js
@@ -186,7 +186,7 @@ async function shareFile() {
 
     let link
     if (fileName.endsWith('.mp4') || fileName.endsWith('.mkv') || fileName.endsWith('.webm') || fileName.endsWith('.mov') || fileName.endsWith('.avi') || fileName.endsWith('.ts') || fileName.endsWith('.ogv')) {
-        link = `${root_url}/file?path=${path}`
+        link = `http://178.128.61.28:8000/file?path=${path}`
     } else {
         link = `${root_url}/file?path=${path}`
 
