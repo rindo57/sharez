6dff0877be981f81abc273a131cbdc9dfc0154fc
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-10-19 01:35:26 +0530

    Update main.py

--- main.py
+++ main.py
@@ -123,9 +123,9 @@ async def api_get_directory(request: Request):
 
     elif "/search_" in data["path"]:
         query = urllib.parse.unquote(data["path"].split("_", 1)[1])
-        path = urllib.parse.unquote(data["filter"].split('/')[1])
+        segments = urllib.parse.unquote(data["path"].split('/'))
+        path = '/'.join(segments[:-1]) 
         print(query)
-        print("path: ",path)
         data = {"contents": DRIVE_DATA.search_file_folder(query, path)}
         print(data)
         folder_data = convert_class_to_dict(data, isObject=False, showtrash=False)
