0c6c70a94fd40a621aa937123fb5952cc6f95b22
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2024-10-18 23:39:38 +0530

    Update main.js

--- website/static/js/main.js
+++ website/static/js/main.js
@@ -1,53 +1,80 @@
 function showDirectory(data) {
-    data = data['contents']
-    document.getElementById('directory-data').innerHTML = ''
-    const isTrash = getCurrentPath().startsWith('/trash')
+    data = data['contents'];
+    document.getElementById('directory-data').innerHTML = '';
+    const isTrash = getCurrentPath().startsWith('/trash');
+    const currentPath = getCurrentPath(); // Get the current path
 
-    let html = ''
+    let html = '';
 
-    // Step 2: Sort the array based on the 'date' values
+    // Filter entries to include only those in the current directory
     let entries = Object.entries(data);
-    let folders = entries.filter(([key, value]) => value.type === 'folder');
-    let files = entries.filter(([key, value]) => value.type === 'file');
+    let folders = entries.filter(([key, value]) => value.type === 'folder' && value.path === currentPath);
+    let files = entries.filter(([key, value]) => value.type === 'file' && value.path === currentPath);
 
-    //folders.sort((a, b) => new Date(b[1].upload_date) - new Date(a[1].upload_date));
-    //files.sort((a, b) => new Date(b[1].upload_date) - new Date(a[1].upload_date));
-    //files.sort((a, b) => a.name.localeCompare(b.name));
+    // Sort folders and files by name
     folders.sort((a, b) => a[1].name.localeCompare(b[1].name));
     files.sort((a, b) => a[1].name.localeCompare(b[1].name));
-    for (const [key, item] of folders) {
-        if (item.type === 'folder') {
-           html += `<tr data-path="${item.path}" data-id="${item.id}" class="body-tr folder-tr"><td><div class="td-align"><img src="static/assets/folder-solid-icon.svg">${item.name}</div></td><td><div class="td-align"></div></td><td><div class="download-btn"></div></td><td><div class="td-align"><a data-id="${item.id}" class="more-btn"><img src="static/assets/more-icon.svg" class="rotate-90"></a></div></td></tr>`
 
-            if (isTrash) {
-                html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options"><input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute"><div id="restore-${item.id}" data-path="${item.path}"><img src="static/assets/load-icon.svg"> Restore</div><hr><div id="delete-${item.id}" data-path="${item.path}"><img src="static/assets/trash-icon.svg"> Delete</div></div>`
-            }
-            else {
-                html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options"><input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute"><div id="rename-${item.id}"><img src="static/assets/pencil-icon.svg"> Rename</div><hr><div id="trash-${item.id}"><img src="static/assets/trash-icon.svg"> Trash</div><hr><div id="folder-share-${item.id}"><img src="static/assets/share-icon.svg"> Share</div></div>`
-            }
+    for (const [key, item] of folders) {
+        html += `<tr data-path="${item.path}" data-id="${item.id}" class="body-tr folder-tr">
+            <td><div class="td-align"><img src="static/assets/folder-solid-icon.svg">${item.name}</div></td>
+            <td><div class="td-align"></div></td>
+            <td><div class="download-btn"></div></td>
+            <td><div class="td-align"><a data-id="${item.id}" class="more-btn"><img src="static/assets/more-icon.svg" class="rotate-90"></a></div></td>
+        </tr>`;
+        
+        // More options for trash and normal directories
+        if (isTrash) {
+            html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options">
+                <input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute">
+                <div id="restore-${item.id}" data-path="${item.path}"><img src="static/assets/load-icon.svg"> Restore</div>
+                <hr>
+                <div id="delete-${item.id}" data-path="${item.path}"><img src="static/assets/trash-icon.svg"> Delete</div>
+            </div>`;
+        } else {
+            html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options">
+                <input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute">
+                <div id="rename-${item.id}"><img src="static/assets/pencil-icon.svg"> Rename</div>
+                <hr>
+                <div id="trash-${item.id}"><img src="static/assets/trash-icon.svg"> Trash</div>
+                <hr>
+                <div id="folder-share-${item.id}"><img src="static/assets/share-icon.svg"> Share</div>
+            </div>`;
         }
     }
 
     for (const [key, item] of files) {
-        if (item.type === 'file') {
-            const size = convertBytes(item.size)
-            html += `<tr data-path="${item.path}" data-id="${item.id}" data-name="${item.name}" class="body-tr file-tr">
-    <td><div class="td-align"><img src="static/assets/file-icon.svg">${item.name}</div></td>
-    <td><div class="td-align">${size}</div></td>
-    <td><div class="td-align"><a href="/file?path=${item.path}/${item.id}" class="download-btn"><img src="static/assets/download-icon.svg" alt="Download"></a></div></td>
-    <td><div class="td-align"><a data-id="${item.id}" class="more-btn"><img src="static/assets/more-icon.svg" class="rotate-90"></a></div></td>
-</tr>`;
-
-            if (isTrash) {
-                html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options"><input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute"><div id="restore-${item.id}" data-path="${item.path}"><img src="static/assets/load-icon.svg"> Restore</div><hr><div id="delete-${item.id}" data-path="${item.path}"><img src="static/assets/trash-icon.svg"> Delete</div></div>`
-            }
-            else {
-                html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options"><input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute"><div id="rename-${item.id}"><img src="static/assets/pencil-icon.svg"> Rename</div><hr><div id="trash-${item.id}"><img src="static/assets/trash-icon.svg"> Trash</div><hr><div id="share-${item.id}"><img src="static/assets/share-icon.svg"> Share</div></div>`
-            }
+        const size = convertBytes(item.size);
+        html += `<tr data-path="${item.path}" data-id="${item.id}" data-name="${item.name}" class="body-tr file-tr">
+            <td><div class="td-align"><img src="static/assets/file-icon.svg">${item.name}</div></td>
+            <td><div class="td-align">${size}</div></td>
+            <td><div class="td-align"><a href="/file?path=${item.path}/${item.id}" class="download-btn"><img src="static/assets/download-icon.svg" alt="Download"></a></div></td>
+            <td><div class="td-align"><a data-id="${item.id}" class="more-btn"><img src="static/assets/more-icon.svg" class="rotate-90"></a></div></td>
+        </tr>`;
+        
+        // More options for trash and normal files
+        if (isTrash) {
+            html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options">
+                <input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute">
+                <div id="restore-${item.id}" data-path="${item.path}"><img src="static/assets/load-icon.svg"> Restore</div>
+                <hr>
+                <div id="delete-${item.id}" data-path="${item.path}"><img src="static/assets/trash-icon.svg"> Delete</div>
+            </div>`;
+        } else {
+            html += `<div data-path="${item.path}" id="more-option-${item.id}" data-name="${item.name}" class="more-options">
+                <input class="more-options-focus" readonly="readonly" style="height:0;width:0;border:none;position:absolute">
+                <div id="rename-${item.id}"><img src="static/assets/pencil-icon.svg"> Rename</div>
+                <hr>
+                <div id="trash-${item.id}"><img src="static/assets/trash-icon.svg"> Trash</div>
+                <hr>
+                <div id="share-${item.id}"><img src="static/assets/share-icon.svg"> Share</div>
+            </div>`;
         }
     }
-    document.getElementById('directory-data').innerHTML = html
 
+    document.getElementById('directory-data').innerHTML = html;
+
+    // Add event listeners for double-click actions
     if (!isTrash) {
         document.querySelectorAll('.folder-tr').forEach(div => {
             div.ondblclick = openFolder;
@@ -60,43 +87,42 @@ function showDirectory(data) {
     document.querySelectorAll('.more-btn').forEach(div => {
         div.addEventListener('click', function (event) {
             event.preventDefault();
-            openMoreButton(div)
+            openMoreButton(div);
         });
     });
 }
 
+// Search functionality
 document.getElementById('search-form').addEventListener('submit', async (event) => {
     event.preventDefault();
     const query = document.getElementById('file-search').value;
-    console.log(query)
+    console.log(query);
     if (query === '') {
         alert('Search field is empty');
         return;
     }
     const path = '/?path=/search_' + encodeURI(query);
-    console.log(path)
+    console.log(path);
     window.location = path;
 });
 
 // Loading Main Page
-
 document.addEventListener('DOMContentLoaded', function () {
-    const inputs = ['new-folder-name', 'rename-name', 'file-search']
+    const inputs = ['new-folder-name', 'rename-name', 'file-search'];
     for (let i = 0; i < inputs.length; i++) {
         document.getElementById(inputs[i]).addEventListener('input', validateInput);
     }
 
     if (getCurrentPath().includes('/share_')) {
-        getCurrentDirectory()
+        getCurrentDirectory();
     } else {
         if (getPassword() === null) {
             document.getElementById('bg-blur').style.zIndex = '2';
             document.getElementById('bg-blur').style.opacity = '0.1';
-
             document.getElementById('get-password').style.zIndex = '3';
             document.getElementById('get-password').style.opacity = '1';
         } else {
-            getCurrentDirectory()
+            getCurrentDirectory();
         }
     }
 });
