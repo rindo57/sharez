5ef052565744f7d2f3c64a93abcc4cfbba2758fc
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-10-31 11:36:34 +0530

    Update downloader.py

--- utils/downloader.py
+++ utils/downloader.py
@@ -84,7 +84,7 @@ async def download_file(url, id, path, filename, singleThreaded):
 
 async def get_file_info_from_url(url):
     username = "AnExt"
-    password = " fhdft783443@"
+    password = "fhdft783443@"
     auth = base64.b64encode(f"{username}:{password}".encode()).decode()
     headers = {
         "Authorization": f"Basic {auth}",
