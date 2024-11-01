4743edde10264b1c2c363220f4d64a6611d5cace
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-01 13:01:20 +0530

    Update downloader.py

--- utils/downloader.py
+++ utils/downloader.py
@@ -4,12 +4,10 @@ import aiohttp, asyncio
 from utils.extra import get_filename
 from utils.logger import Logger
 from pathlib import Path
+from bs4 import BeautifulSoup
 from utils.uploader import start_file_uploader
 from techzdl import TechZDL
-import requests
-from bs4 import BeautifulSoup
-import os
-
+import requests 
 logger = Logger(__name__)
 
 DOWNLOAD_PROGRESS = {}
@@ -33,63 +31,47 @@ async def download_file(url, id, path, filename, singleThreaded):
     global DOWNLOAD_PROGRESS, STOP_DOWNLOAD
 
     logger.info(f"Downloading file from {url}")
-    username = "AnExt"
-    password = "fhdft783443@"
-    auth = base64.b64encode(f"{username}:{password}".encode()).decode()
-    
-    headers = {
-        "Authorization": f"Basic {auth}",
-        "Referer": "https://void.anidl.org",
-        "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
-    }
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            if href.endswith('.mkv'):
-                file_url = "https://void.anidl.org" + link['href']
-                try:
-                    downloader = TechZDL(
-                        file_url,
-                        output_dir=cache_dir,
-                        debug=False,
-                        progress_callback=download_progress_callback,
-                        progress_args=(id,),
-                        max_retries=5,
-                        single_threaded=singleThreaded,
-                        custom_headers=headers,
-                    )
-                    await downloader.start(in_background=True)
 
-                    await asyncio.sleep(5)
+    try:
+        downloader = TechZDL(
+            url,
+            output_dir=cache_dir,
+            debug=False,
+            progress_callback=download_progress_callback,
+            progress_args=(id,),
+            max_retries=5,
+            single_threaded=singleThreaded,
+        )
+        await downloader.start(in_background=True)
 
-                    while downloader.is_running:
-                        if id in STOP_DOWNLOAD:
-                            logger.info(f"Stopping download {id}")
-                            await downloader.stop()
-                            return
-                        await asyncio.sleep(1)
+        await asyncio.sleep(5)
 
-                    if downloader.download_success is False:
-                        raise downloader.download_error
+        while downloader.is_running:
+            if id in STOP_DOWNLOAD:
+                logger.info(f"Stopping download {id}")
+                await downloader.stop()
+                return
+            await asyncio.sleep(1)
 
-                    DOWNLOAD_PROGRESS[id] = (
-                        "completed",
-                        downloader.total_size,
-                        downloader.total_size,
-                    )
+        if downloader.download_success is False:
+            raise downloader.download_error
 
-                    logger.info(f"File downloaded to {downloader.output_path}")
+        DOWNLOAD_PROGRESS[id] = (
+            "completed",
+            downloader.total_size,
+            downloader.total_size,
+        )
 
-                    asyncio.create_task(
-                        start_file_uploader(
-                            downloader.output_path, id, path, filename, downloader.total_size
-                        )
-                    )
-                except Exception as e:
-                    DOWNLOAD_PROGRESS[id] = ("error", 0, 0)
-                    logger.error(f"Failed to download file: {url} {e}")
+        logger.info(f"File downloaded to {downloader.output_path}")
+
+        asyncio.create_task(
+            start_file_uploader(
+                downloader.output_path, id, path, filename, downloader.total_size
+            )
+        )
+    except Exception as e:
+        DOWNLOAD_PROGRESS[id] = ("error", 0, 0)
+        logger.error(f"Failed to download file: {url} {e}")
 
 
 async def get_file_info_from_url(url):
@@ -101,26 +83,14 @@ async def get_file_info_from_url(url):
         "Referer": "https://void.anidl.org",
         "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/58.0.3029.110 Safari/537.36",
     }
-    response = requests.get(url, auth=(username, password))
-    if response.status_code == 200:
-        soup = BeautifulSoup(response.text, 'html.parser')
-        
-        # Loop through each link to find the .mkv file
-        for link in soup.find_all('a', href=True):
-            href = link['href']
-            if href.endswith('.mkv'):
-                file_url = "https://void.anidl.org" + href
-                downloader = TechZDL(
-                    file_url,
-                    output_dir=cache_dir,
-                    debug=False,
-                    progress_callback=download_progress_callback,
-                    progress_args=(id,),  # Be sure 'id' is defined in this scope
-                    max_retries=5,
-                    custom_headers=headers,
-                )
-                file_info = await downloader.get_file_info()
-                return {"file_size": file_info["total_size"], "file_name": file_info["filename"]}
-    
-    # In case no .mkv file is found or request fails
-    return None
+    downloader = TechZDL(
+        url,
+        output_dir=cache_dir,
+        debug=False,
+        progress_callback=download_progress_callback,
+        progress_args=(id,),
+        max_retries=5,
+        custom_headers=headers,
+    )
+    file_info = await downloader.get_file_info()
+    return {"file_size": file_info["total_size"], "file_name": file_info["filename"]}
