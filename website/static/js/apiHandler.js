c694e4cff2acf3b52a902083f9f36ec0651fa33e
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-02 20:35:02 +0530

    Update apiHandler.js

--- website/static/js/apiHandler.js
+++ website/static/js/apiHandler.js
@@ -49,6 +49,14 @@ async function getCurrentDirectory() {
                     sections[0].setAttribute('class', 'unselected-item');
                 }
                 sections[0].href = `/?path=/share_${removeSlash(json['auth_home_path'])}&auth=${auth}`;
+            } else if (getCurrentPath().includes('/search_')) {
+                const sections = document.querySelector('.sidebar-menu').getElementsByTagName('a');
+
+                if (removeSlash(json['auth_home_path']) === removeSlash(path.split('_')[1])) {
+                    sections[0].setAttribute('class', 'selected-item');
+                } else {
+                    sections[0].setAttribute('class', 'unselected-item');
+                }
             }
 
             showDirectory(json['data']);
