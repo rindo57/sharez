10928b0ea2fcf839713e8c0898073c8a96d9e8ef
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-07 01:17:41 +0530

    Update extra.js

--- website/static/js/extra.js
+++ website/static/js/extra.js
@@ -109,7 +109,7 @@ function getPassword() {
 }
 
 function getRandomId() {
-    const length = 6;
+    const length = 30;
     const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789';
     let result = '';
     for (let i = 0; i < length; i++) {
