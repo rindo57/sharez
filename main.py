d5b9642805c46d84a8572ee32f34c009824535b8
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-09 18:39:14 +0530

    Update main.py

--- main.py
+++ main.py
@@ -274,7 +274,7 @@ async def generate_link_page(download_path: str):
       <title>URL Verification</title>
       <style>
         body {{ font-family: 'Arial', sans-serif; display: flex; justify-content: center; align-items: center; min-height: 100vh; background-color: #25293c; }}
-        .container {{ background: #3d405c; padding: 2rem; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); max-width: 400px; width: 100%; }}
+        .container {{ background: #1b1f2f; padding: 2rem; border-radius: 8px; box-shadow: 0 2px 10px rgba(0, 0, 0, 0.1); max-width: 400px; width: 100%; }}
         h2 {{ margin-bottom: 1rem; color: white; }}
         p {{ margin-bottom: 1rem; color: white; }}
         button {{ padding: 0.7rem; background-color: #ff79c6; color: white; border: none; border-radius: 4px; cursor: pointer; }}
