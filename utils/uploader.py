bc744f87e9f88e420a46f2378ddfa6fa568b31d7
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-17 23:11:35 +0530

    Update uploader.py

--- utils/uploader.py
+++ utils/uploader.py
@@ -7,6 +7,14 @@ from utils.logger import Logger
 from urllib.parse import unquote_plus
 from pymediainfo import MediaInfo
 import requests
+import os
+import base64
+import aiohttp, asyncio
+from http.cookies import SimpleCookie
+from json import loads as json_loads
+import urllib.parse
+import urllib.request
+import http.cookiejar
 logger = Logger(__name__)
 PROGRESS_CACHE = {}
 STOP_TRANSMISSION = []
@@ -24,21 +32,55 @@ async def progress_callback(current, total, id, client: Client, file_path):
         except:
             pass
 
-def upload_to_rentry(content):
-    try:
-        response = requests.post(
-            "https://rentry.co/api/new",
-            data={"text": content, "lang": "Plain Text"},
-        )
-        if response.status_code == 200:
-            rentry_data = response.json()
-            return f"https://rentry.co/{rentry_data['url']}"
-        else:
-            logger.error("Failed to upload to rentry.co")
-            return None
-    except Exception as e:
-        logger.error(f"Error uploading to rentry.co: {e}")
-        return None
+
+
+_headers = {"Referer": 'https://rentry.co'}
+
+# Simple HTTP Session Client, keeps cookies
+class UrllibClient:
+    def __init__(self):
+        self.cookie_jar = http.cookiejar.CookieJar()
+        self.opener = urllib.request.build_opener(urllib.request.HTTPCookieProcessor(self.cookie_jar))
+        urllib.request.install_opener(self.opener)
+
+    def get(self, url, headers={}):
+        request = urllib.request.Request(url, headers=headers)
+        return self._request(request)
+
+    def post(self, url, data=None, headers={}):
+        postdata = urllib.parse.urlencode(data).encode()
+        request = urllib.request.Request(url, postdata, headers)
+        return self._request(request)
+
+    def _request(self, request):
+        response = self.opener.open(request)
+        response.status_code = response.getcode()
+        response.data = response.read().decode('utf-8')
+        return response
+
+
+def new(url, edit_code, text):
+    client, cookie = UrllibClient(), SimpleCookie()
+    cookie.load(vars(client.get('https://rentry.co'))['headers']['Set-Cookie'])
+    csrftoken = cookie['csrftoken'].value
+
+    payload = {
+        'csrfmiddlewaretoken': csrftoken,
+        'url': url,
+        'edit_code': edit_code,
+        'text': text
+    }
+    return json_loads(client.post('https://rentry.co/api/new', payload, headers=_headers).data)
+
+
+def get_rentry_link(text):
+    url, edit_code = '', ''
+    response = new(url, edit_code, text)
+    if response['status'] == '200':
+        return f"https://rentry.co/{response['url']}"
+    else:
+        raise Exception(f"Rentry API Error: {response['content']}")
+        
 async def start_file_uploader(file_path, id, directory_path, filename, file_size):
     global PROGRESS_CACHE
     from utils.directoryHandler import DRIVE_DATA
@@ -56,7 +98,7 @@ async def start_file_uploader(file_path, id, directory_path, filename, file_size
         f"\nMedia Info:\n{media_details}"
     )
 
-    rentry_link = upload_to_rentry(content)
+    rentry_link = get_rentry_link(content)
     print(rentry_link)
     if file_size > 1.98 * 1024 * 1024 * 1024:
         # Use premium client for files larger than 2 GB
