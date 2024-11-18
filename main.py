2affd20e6371a38d2249b51485d94971d5152aa4
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-18 16:11:09 +0530

    Update main.py

--- main.py
+++ main.py
@@ -47,6 +47,13 @@ async def lifespan(app: FastAPI):
 
 
 app = FastAPI(docs_url=None, redoc_url=None, lifespan=lifespan)
+@app.middleware("http")
+async def add_cache_control_headers(request, call_next):
+    response = await call_next(request)
+    response.headers["Cache-Control"] = "no-store, no-cache, must-revalidate, max-age=0"
+    response.headers["Pragma"] = "no-cache"
+    response.headers["Expires"] = "0"
+    return response
 logger = Logger(__name__)
 
 '''security = HTTPBasic()
