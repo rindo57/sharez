5f2d40b434ca1aed819818b27fc38bac816dbeba
rindo57 <77073061+rindo57@users.noreply.github.com>
2024-11-20 19:02:22 +0530

    Update extra.js

--- website/static/js/extra.js
+++ website/static/js/extra.js
@@ -108,14 +108,14 @@ function getPassword() {
     return localStorage.getItem('password')
 }
 
-async function posJson(url, data) {
+async function posJson(url) {
   //  data['password'] = getPassword();
     const response = await fetch(url, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
+        }
+        
     });
     return await response.json();
 }
