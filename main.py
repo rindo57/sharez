d363afde787947c2dc6f9831fb3e670a3469c432
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-07 10:08:36 +0530

    Update main.py

--- main.py
+++ main.py
@@ -365,7 +365,10 @@ async def generate_link_page(request: Request):
     filesize = convert_size(file.size)  # Convert to MB/GB/etc.
     views = stats["views"] + 1  # Increment view for this request
     downloads = stats["downloads"]
-    media_info = file.paste_url
+    if not file.paste_url:
+        media_info = file.rentry_link
+    else:
+        media_info = file.paste_url
     uploader = file.uploader
     return HTMLResponse(content=f"""
 <!DOCTYPE html>
