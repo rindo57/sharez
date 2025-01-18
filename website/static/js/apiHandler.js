49941097f5cf1c4ee997bd963fbba10e65758080
rindo57 <77073061+rindo57@users.noreply.github.com>
2025-01-18 12:11:40 +0530

    Update apiHandler.js

--- website/static/js/apiHandler.js
+++ website/static/js/apiHandler.js
@@ -146,7 +146,7 @@ function hideMoreColumnIfSharedPath() {
     }
 }
 async function getCurrentDirectory() {
-    let path = getSharePath();
+    let path = getCurrentPath();
     if (path === 'redirect') {
         return;
     }
